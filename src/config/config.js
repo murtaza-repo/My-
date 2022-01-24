02c3e88dc2fe8f7a39bdf8e0f3f0d9a7c99de6e3
Saiftechie <murtazakhachrod@gmail.com>
2022-01-24 15:27:23 +0530

    add new link

--- src/config/config.js
+++ src/config/config.js
@@ -36,7 +36,7 @@ export const configuration = {
     Portfolio:{
         PortfolioTitle:'my work',
         PortfolioItems:[
-            {image:collegifyImg,name:'Collegify App',des:'A Modern Javascript Application developed to provide common platform to students where they can find inter-college events and placements/campus-drives details and register for the same via App.',link:'https://wwwsaiftechtk.000webhostapp.com/'},
+            {image:collegifyImg,name:'Collegify App',des:'A Modern Javascript Application developed to provide common platform to students where they can find inter-college events and placements/campus-drives details and register for the same via App.',link:'https://collegify-me.web.app/'},
             {image:todoApp,name:'Todo App',des:'A compact todo app created using latest React features that makes it handy to use.',link:'https://todoappreact53.web.app/'},
             {image:hms,name:'HMS App',des:'A Hospital Managment System design and developed using Python Flask Framework. This project comprises of self-created APIs endpoints that guides the App flow. It uses Flask SQlAlchemy as the database library which is based on object manipulation which makes the project easy to integrate in an Object Oriented Environment.', link:'https://murtaza.pythonanywhere.com/'},
             {image:recipeBook,name:'RecipeBook App',des:'An Angular CRUD App which allow users to Create, Read, Update & Delete Recipes and Ingredients required for respective recipes. It also help to create a shopping list with an ease by simply going through recipes.',link:'https://angularrecipebook-a0067.web.app/'},
